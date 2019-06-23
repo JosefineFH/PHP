59074672a2bc8fcd0b02380750dfe196f87dc34b
Josefine Magnussen <josefine.magnussen@hotmail.com>
2019-06-23 22:12:32 +0200

    Created cabins.php

---
+++ BookingApp/cabins.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <style>
+<?php include 'includes/css/cabins.css'; ?>
+</style>
+<body>
+    
+<div class="grid-container">
+
+<div class="col1">
+<?php include("includes/navbar.html");?>
+</div>
+
+<div class="col2">
+
+<p> Cabin 1</p>
+<img src="includes/imag/cabin2_1.jpg" alt="">
+
+</div>
+
+<div class="col3">
+<p>This is us </p>
+<button> Order </button>
+</div>
+
+<div class="col4">
+</div>
+</div>
+<style>
+img{
+    height: 400px;
+    display: block;
+    margin-left: 10%;
+    }
+    </style>
+</body>
+
+</html>
