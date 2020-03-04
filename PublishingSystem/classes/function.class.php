a47eb55dd4c2a3ed8ea0b0a906260212ba8921d9
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-03-04 19:39:10 +0100

    new function

--- PublishingSystem/classes/function.class.php
+++ PublishingSystem/classes/function.class.php
@@ -4,6 +4,11 @@ require_once('DBConnection.class.php');
 
 class functions extends DBConnection {
 
+    public function getUserFullbane($userId){
+        $sqlStatement = "SELECT fullname FROM users WHERE userid = ?";
+        $result = $this->doQuery($sqlStatement, $userId);
+        return $result;
+    }
     public function getArticles($userId)
     {
         $query = "SELECT article.title, users.fullname, article.id FROM article INNER JOIN users ON article.userid = users.userid";
