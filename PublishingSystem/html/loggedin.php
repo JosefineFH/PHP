b6cff10691efbc224a859a207a216049878ac567
Josefine Magnussen <josefine.magnussen@hotmail.com>
2020-03-04 19:41:14 +0100

    cleaned up code

--- PublishingSystem/html/loggedin.php
+++ PublishingSystem/html/loggedin.php
@@ -9,6 +9,10 @@ include_once('../classes/function.class.php');
 
 $getArticles = new functions();
 $userId = explode(" ", $_SESSION['user']);
+
+$getUserFullname = new functions();
+$fullUserName = $getUserFullname->getUserFullbane($userId);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,43 +21,53 @@ $userId = explode(" ", $_SESSION['user']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../includes/css/style.css" type="text/css">
-
-    <h1>Logedin</h1>
 </head>
 
 <body>
-    <a href="logout.php" class="btn btn-danger"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
 
-    <table width="100%" border="0">
-        <tr>
-            <th>Title</th>
-            <th>content</th>
-        </tr>
-        <?php
-        $sql = $getArticles->getArticles($userId);
-        $cnt = 1;
-        foreach ($sql as $data) {
-        ?>
-            <tr>
-                <td>
-                    <a href="edit.php?id=<?php echo htmlentities($data['id']); ?>">
-                        <?php echo $data['title']; ?>
-                    </a>
+    <div class="article">
+        <section>
+            <!-- Prints full name of the user who is logged in -->
+            <h1>Welcomes <?php echo $fullUserName[0]['fullname']; ?></h1>
+        </section>
 
-                </td>
-                <td>
-                    <?php echo $data['fullname']; ?>
-                </td>
+        <section>
+            <table>
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                </tr>
 
-            </tr>
-        <?php
-            // for serial number increment
-            $cnt++;
-        } ?>
-        </tbody>
-    </table>
+                <?php
+                $sql = $getArticles->getArticles($userId);
+                $count = 1;
 
-    </table>
+                foreach ($sql as $data) {
+                ?>
+
+                    <tr>
+                        <td>
+                            <a href="edit.php?id=<?php echo htmlentities($data['id']); ?>">
+                                <?php echo $data['title']; ?>
+                            </a>
+
+                        </td>
+                        <td>
+                            <?php echo $data['fullname']; ?>
+                        </td>
+
+                    </tr>
+                <?php
+                    // for serial number increment
+                    $count++;
+                } ?>
+            </table>
+        </section>
+
+        <section>
+            <a href="logout.php" class="button"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+        </section>
+    </div>
 </body>
 
 </html>
